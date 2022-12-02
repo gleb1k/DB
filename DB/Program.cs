b761f1b68d4a304798a12ad4c922dd3587e6fcbe
Gleb Gafeev <gleb.gafeev@mail.ru>
2022-12-03 00:48:40 +0300

    Serializable exeption (hw9?)

--- DB/Program.cs
+++ DB/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using DB.Lesson2;
+using Npgsql;
 using DB.HW2;
 namespace DB
 {
@@ -11,8 +12,32 @@ namespace DB
             //Lesson2Runner.Run();
 
             //Json/XML - HW2    
-            HW2Runner.Run();
+            //HW2Runner.Run();
 
+            var connectionString = "Host=localhost;Username=postgres;Password=12345678;Database=taxi";
+            using var connection = new NpgsqlConnection(connectionString);
+            
+            connection.Open();
+            
+            using var cmd = new NpgsqlCommand();
+            cmd.Connection = connection;
+            try
+            {
+                cmd.Transaction = connection.BeginTransaction(System.Data.IsolationLevel.Serializable);
+                cmd.CommandText = "update car set pasport = '111' where brand ='lada' ";
+                cmd.ExecuteNonQuery();
+
+                cmd.Transaction = connection.BeginTransaction(System.Data.IsolationLevel.Serializable);
+                cmd.CommandText = "update car set pasport = '222' where brand ='lada'";
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine(ex.Message);
+            }
+            
+            
         }
 
     }
